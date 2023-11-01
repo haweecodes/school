9da68f0e377d3f0187f5ac04a68d1c00dfbc15d1
haweecodes <haweesajib10@gmail.com>
2023-11-01 23:27:57 +0600

    chore: i18n config added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/bn/product/',
+        permanent: false,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
